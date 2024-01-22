46e592dc5772bee095369522a9a7e188f8dc9bdc
Maciej Krüger <mkg20001@gmail.com>
2024-01-23 00:38:05 +0100

    fix version

--- version.sh
+++ version.sh
@@ -3,5 +3,5 @@
 
 set -euo pipefail
 
-VERSION=$(git show --oneline  --pretty=format:'%h %ad [%an] %s' | head -n 1)
+VERSION=$(git log --pretty=format:'%h %ad [%an] %s' HEAD~...HEAD)
 sed -i "2i version: $VERSION" README.md
