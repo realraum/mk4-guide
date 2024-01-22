4d477e58f9a5340bfcda40b2510defd881da124c
Maciej Krüger <mkg20001@gmail.com>
2024-01-23 00:31:01 +0100

    fix

--- version.sh
+++ version.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p git
+
+set -euo pipefail
 
 VERSION=$(git log --pretty=format:'%h %ad [%an] %s' HEAD~...HEAD)
 sed -i "2i version: $VERSION" README.md
